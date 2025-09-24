1885726a4e654451af54aff070d9ec949ca4bfa3
sotomiguela09 <sotomiguela09@gmail.com>
2025-09-24 16:40:09 -0400

    ejercicio 5 numero positivo

---
+++ ejercicio5/ejercicio5/Controllers/NumeroController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejercicio5.Controllers
+{
+    public class NumeroController1 : Controller
+
+    {
+        public ActionResult Signo(int numero)
+        {
+            string resultado;
+            if (numero > 0) resultado = "El número es positivo.";
+            else if (numero < 0) resultado = "El número es negativo.";
+            else resultado = "El número es cero.";
+
+            ViewBag.Numero = numero;
+            ViewBag.Resultado = resultado;
+            {
+                return View();
+            }
+        }
+    }
+}
