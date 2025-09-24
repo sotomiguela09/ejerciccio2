b0b8f5c2628396149f4107991ea1b35251410822
sotomiguela09 <sotomiguela09@gmail.com>
2025-09-23 22:20:18 -0400

    Agregar archivos de proyecto.

---
+++ ejercicio 2/Controllers/PresentacionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejercicio_2.Controllers
+{
+    public class PresentacionController : Controller
+    {
+        public ActionResult Saludo()
+        {
+
+            return View();
+
+
+        }
+    }
+}
