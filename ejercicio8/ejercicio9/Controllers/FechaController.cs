6d51999c20167df62a1e1b853c190e133609694d
sotomiguela09 <sotomiguela09@gmail.com>
2025-09-25 17:34:10 -0400

    ejercicio 9 Dia de semana

---
+++ ejercicio8/ejercicio9/Controllers/FechaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejercicio9.Controllers
+{
+    public class FechaController : Controller
+    {
+ 
+            public IActionResult DiaSemana(DateTime fecha)
+            {
+                string dia = fecha.ToString("dddd");
+                ViewBag.Mensaje = $"La fecha {fecha.ToShortDateString()} corresponde a {dia}.";
+                return View();
+            }
+        }
+    }
+
+
