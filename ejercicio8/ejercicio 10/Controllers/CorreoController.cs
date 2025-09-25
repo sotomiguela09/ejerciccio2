716decb0eb6e230c455bef47f0caefab2c464e61
sotomiguela09 <sotomiguela09@gmail.com>
2025-09-25 19:32:47 -0400

    ejercicio 10 correo

---
+++ ejercicio8/ejercicio 10/Controllers/CorreoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejercicio_10.Controllers
+{
+    public class CorreoController : Controller
+
+
+    {
+        public IActionResult Usuario(string email)
+        {
+            string usuario = email.Split('@')[0];
+            ViewBag.Mensaje = $"El usuario del correo {email} es: {usuario}";
+            return View();
+        }
+    }
+}
