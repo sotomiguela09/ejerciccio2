0c0f34f0e58f5f95e4e9dc4447c6d106389cccda
sotomiguela09 <sotomiguela09@gmail.com>
2025-09-25 20:21:28 -0400

    EJERCICIO11

---
+++ ejercicio8/Ejercicio11/Controllers/SeguridadController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio11.Controllers{
+    using System.Text.RegularExpressions;
+
+    public class SeguridadController : Controller
+    {
+     
+
+
+
+        
+public IActionResult ClaveFuerte(string clave)
+        {
+            bool esFuerte = clave.Length >= 8 &&
+                            Regex.IsMatch(clave, "[A-Z]") &&
+                            Regex.IsMatch(clave, "[a-z]") &&
+                            Regex.IsMatch(clave, "[0-9]");
+
+            ViewBag.Mensaje = esFuerte ? "La clave es segura ✅" : "La clave NO es segura ❌";
+            return View();
+
+        }
+    }
+}
+
+            
